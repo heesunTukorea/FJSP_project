1781dbbe3690f2f9b91aa5442ffb1d094f87996d
HeeeSun Yang <jy990311@naver.com>
2023-08-02 00:19:34 +0900

    1212211

--- pages/data_set_code.py
+++ pages/data_set_code.py
@@ -6,127 +6,383 @@ col1, col2 = st.columns([7,1])
 st.title("테이터셋 코드")
 with col2:
     st.image("tuk_img.png")
-code = '''
-import pandas as pd
-import random as rd
-import numpy as np
-import streamlit as st
 
-#job의 이름과 operation생성 파일
-#count에 job의 갯수 할당
-#랜덤범위 1~7
-@st.cache_data
-def job(count,jmin,jmax):
-    F_job=[]
-    for i in range(1,count+1):
-        job_number = i
-        job_type = rd.randrange(jmin,jmax+1)#난수 범위
-        F_job.append([job_number,job_type])
-    columns = ['','job_type']
-    pd_F_job = pd.DataFrame(F_job, columns = columns)
-    pd_F_job.index = np.arange(1, len(pd_F_job) + 1)
-    pd_F_job.to_csv('FJSP_Job.csv', index = False)
+with st.sidebar:
+    selecop = st.selectbox('코드 선택', ('함수 코드','페이지 코드'))
+if selecop == '함수 코드':
+    code = '''
+    import pandas as pd
+    import random as rd
+    import numpy as np
+    import streamlit as st
+    import matplotlib.pyplot as plt
+    import os
 
-#job들의 setup타임 생성
-#랜덤범위 1~10
-@st.cache_data
-def setup(smin,smax):
-    job_df = pd.read_csv('FJSP_Job.csv', index_col=False)
-    job_df.columns = ['job_num','job_type']
-    #고유값들의 수를 뽑아서 시뮬레이터 데이터 생성
-    job_num = job_df['job_num'].nunique()+1
-    set_df = pd.DataFrame(np.random.randint(smin, smax+1, size=(job_num, job_num)))#난수범위
-    
-    #컬럼과 컬럼 이름 변경
-    columns = job_df['job_num'].unique()
-    pd_F_set = pd.DataFrame(set_df, index=columns, columns=columns)
-    pd_F_set = pd_F_set.add_prefix('j')
-    pd_F_set.index = pd_F_set.index.astype(str)
-    pd_F_set.index = 'j'+ pd_F_set.index 
-    
-    #인덱스와 컬럼이 같을 때 값을 0으로 변경
-    np.fill_diagonal(pd_F_set.values, 0)
-    pd_F_set.to_csv('FJSP_Set.csv')
+    #job의 이름과 operation생성 파일
+    #count에 job의 갯수 할당
+    #랜덤범위 1~7
+    @st.cache_data
+    def job(count,jmin,jmax):
+        F_job=[]
+        for i in range(1,count+1):
+            job_number = i
+            job_type = rd.randrange(jmin,jmax+1)#난수 범위
+            F_job.append([job_number,job_type])
+        columns = ['','number']
+        pd_F_job = pd.DataFrame(F_job, columns = columns)
+        pd_F_job.index = np.arange(1, len(pd_F_job) + 1)
+        pd_F_job.to_csv('FJSP_Job.csv', index = False)
 
-    #머신과 job,operation의 process시간
-#랜덤범위5~25
-@st.cache_data
-def sim(machine_count,pmin,pmax,opmin,opmax):
-    
-    machine = np.array(range(1, machine_count+1))
-    job_df = pd.read_csv('FJSP_Job.csv', index_col=False)
-    job_df.columns = ['job_num','job_type']
-    job_num = job_df['job_num'].nunique()
-    jo = []
-    
-    #오퍼레이션 랜덤 생성
-    job_df['job_operation'] = np.random.randint(opmin, opmax+1, size=(job_num, 1))#난수범위
-    job_df_op = job_df['job_operation']
-    
-    # 작업 번호와 작업의 공정 수를 이용하여 작업 이름을 생성
-    for index, row in job_df.iterrows():
-        job_num = row['job_num']
-        job_operation = row['job_operation']
-        for j in range(1, job_operation + 1):
-            b = "j" + str(job_num).zfill(2) + str(j).zfill(2)
-            jo.append(b)
-            
-    # 작업 이름과 기계 번호를 이용하여 시뮬레이션 데이터프레임 생성
-    sim_df = pd.DataFrame(np.random.randint(pmin, pmax+1, size=(len(jo), machine_count)), index=jo, columns=machine)
-    
-    # 컬럼 이름에 'M' 접두사 추가
-    pd_sim = sim_df.add_prefix('M')
-    
-    pd_sim.to_csv('FJSP_Sim.csv', index=True, header=True)
+    #job들의 setup타임 생성
+    #랜덤범위 1~10
+    @st.cache_data
+    def setup(smin,smax):
+        job_df = pd.read_csv('FJSP_Job.csv', index_col=False)
+        job_df.columns = ['job_num','job_type']
+        #고유값들의 수를 뽑아서 시뮬레이터 데이터 생성
+        job_num = job_df['job_num'].nunique()+1
+        set_df = pd.DataFrame(np.random.randint(smin, smax+1, size=(job_num, job_num)))#난수범위
+        
+        #컬럼과 컬럼 이름 변경
+        columns = job_df['job_num'].unique()
+        pd_F_set = pd.DataFrame(set_df, index=columns, columns=columns)
+        pd_F_set = pd_F_set.add_prefix('j')
+        pd_F_set.index = pd_F_set.index.astype(str)
+        pd_F_set.index = 'j'+ pd_F_set.index 
+        
+        #인덱스와 컬럼이 같을 때 값을 0으로 변경
+        np.fill_diagonal(pd_F_set.values, 0)
+        pd_F_set.to_csv('FJSP_Set.csv')
 
-#Q_time 생성 코드
-@st.cache_data
-def Q_time(qmin,qmax):
-    job_df = pd.read_csv('FJSP_Job.csv', index_col=0)
-    job_pro = pd.read_csv('FJSP_Sim.csv', index_col=0)
-    
-    job_pro_index = job_pro.index
-    counts = []
-    current_count = 1
-    # processing 데이터에서 각 작업의 공정 수 리스트를 추출
-    for i in range(1, len(job_pro_index)):
-        if job_pro_index[i][:3] == job_pro_index[i-1][:3]:
-            current_count += 1
-        else:
-            counts.append(current_count)
-            current_count = 1
+        #머신과 job,operation의 process시간
+    #랜덤범위5~25
+    @st.cache_data
+    def sim(machine_count,pmin,pmax,opmin,opmax):
+        
+        machine = np.array(range(1, machine_count+1))
+        job_df = pd.read_csv('FJSP_Job.csv', index_col=False)
+        job_df.columns = ['job_num','job_type']
+        job_num = job_df['job_num'].nunique()
+        jo = []
+        
+        #오퍼레이션 랜덤 생성
+        job_df['job_operation'] = np.random.randint(opmin, opmax+1, size=(job_num, 1))#난수범위
+        job_df_op = job_df['job_operation']
+        
+        # 작업 번호와 작업의 공정 수를 이용하여 작업 이름을 생성
+        for index, row in job_df.iterrows():
+            job_num = row['job_num']
+            job_operation = row['job_operation']
+            for j in range(1, job_operation + 1):
+                b = "j" + str(job_num).zfill(2) + str(j).zfill(2)
+                jo.append(b)
+                
+        # 작업 이름과 기계 번호를 이용하여 시뮬레이션 데이터프레임 생성
+        sim_df = pd.DataFrame(np.random.randint(pmin, pmax+1, size=(len(jo), machine_count)), index=jo, columns=machine)
+        
+        # 컬럼 이름에 'M' 접두사 추가
+        pd_sim = sim_df.add_prefix('M')
+        
+        pd_sim.to_csv('FJSP_Sim.csv', index=True, header=True)
 
-    counts.append(current_count)
-    job_df_op = counts
-    
-    #공정수 리스트의 최댓값과 리스트 생성
-    job_df_op_max = max(job_df_op)
-    job_df_op_max_1 = list(range(1,job_df_op_max+1))
-    job_df_op_values = job_df_op
+    #Q_time 을 생성하는 코드
+    @st.cache_data
+    def Q_time(qmin,qmax):
+        #기존 csv파일을 불러와 데이터를 사용
+        job_df = pd.read_csv('FJSP_Job.csv', index_col=0)
+        job_pro = pd.read_csv('FJSP_Sim.csv', index_col=0)
+        
+        job_pro_index = job_pro.index
+        counts = []
+        current_count = 1
 
-    j_op = job_df.index
-    j_op = j_op.astype(str)
-    j_op1 = 'j'+ j_op 
-    j_op_num = j_op1.nunique()
-
-
-    columns = job_df_op_max_1
-    index = j_op1
-    #빈 데이터 프레임 생성
-    q_time = pd.DataFrame(index=range(j_op_num), columns=job_df_op_max_1)  # 빈 데이터프레임 생성
-    
-    #공정수 리스트를 려서 내용은 공정의 최댓값에서 범위를 지정해 랜덤의 값으로 입력 
-    for i, val in enumerate(job_df_op_values):
-        for j in range(job_df_op_max):
-            if j < val:
-                q_time.iloc[i, j] = int(np.random.uniform(qmin,qmax) * job_pro.iloc[i].max())
-
+        #프로세싱 타이임의 인덱스를 추출해서 작업과 공저의 갯수 추출
+        for i in range(1, len(job_pro_index)):
+            if job_pro_index[i][:3] == job_pro_index[i-1][:3]:
+                current_count += 1
             else:
-                q_time.iloc[i, j] = 0
+                counts.append(current_count)
+                current_count = 1
 
-    q_time.index = np.arange(1, len(q_time) + 1)
-    q_time.index = q_time.index.astype(str)
-    q_time.index = 'j'+ q_time.index
+        counts.append(current_count)
+        job_df_op = counts
+        
+        #
+        job_df_op_max = max(job_df_op)
+        job_df_op_max_1 = list(range(1,job_df_op_max+1))
+        job_df_op_values = job_df_op
 
-    q_time.to_csv('FJSP_Q_time.csv', index=True, header=True)'''
-st.code(code, language='python')
+        j_op = job_df.index
+        j_op = j_op.astype(str)
+        j_op1 = 'j'+ j_op 
+        j_op_num = j_op1.nunique()
+
+        #Q_time을 기계들의 processing time의 최댓값에서 범위를 지정해서 곱함
+        #공정이 없는것은 0으로 처리
+        columns = job_df_op_max_1
+        index = j_op1
+        q_time = pd.DataFrame(index=range(j_op_num), columns=job_df_op_max_1)  # 빈 데이터프레임 생성
+
+        for i, val in enumerate(job_df_op_values):
+            for j in range(job_df_op_max):
+                if j < val:
+                    q_time.iloc[i, j] = int(np.random.uniform(qmin,qmax) * job_pro.iloc[i].max())
+
+                else:
+                    q_time.iloc[i, j] = 0
+
+        q_time.index = np.arange(1, len(q_time) + 1)
+        q_time.index = q_time.index.astype(str)
+        q_time.index = 'j'+ q_time.index
+
+        q_time.to_csv('FJSP_Q_time.csv', index=True, header=True)
+
+
+    #기계와 공정의 오류를 생성하는 코드
+    @st.cache_data
+    def add_unavailable_machines_to_sim(sim_file, unavailable_machine_options=None):
+        sim_df = pd.read_csv(sim_file, index_col=0)
+
+        job_pro = sim_df
+        job_pro_index = job_pro.index
+        counts = []
+        current_count = 1
+
+        #프로세싱 타임에서 작업과 공정의 수를 추출
+        for i in range(1, len(job_pro_index)):
+            if job_pro_index[i][:3] == job_pro_index[i-1][:3]:
+                current_count += 1
+            else:
+                counts.append(current_count)
+                current_count = 1
+
+        counts.append(current_count)
+        job_df_op = counts
+        
+        #기계와 공정을 지정해서 오류를 가정해 0을 입력
+        if unavailable_machine_options:
+            valid_options = []
+            for machine, job, operation in unavailable_machine_options:
+                if job in range(1, len(job_df_op) + 1):  # 유효한 작업 번호만 추가
+                    valid_options.append([machine, job, operation])
+
+            for machine, job, operation in valid_options:
+                if operation is None:
+                    for operation1 in range(1, job_df_op[job - 1] + 1):
+                        job_key = 'j' + str(job).zfill(2) + str(operation1).zfill(2)
+                        machine_name = 'M' + str(machine)
+                        sim_df.loc[job_key, machine_name] = 0
+                else:
+                    job_key = 'j' + str(job).zfill(2) + str(operation).zfill(2)
+                    machine_name = 'M' + str(machine)
+                    sim_df.loc[job_key, machine_name] = 0  # 해당 작업의 해당 공정에 해당하는 기계 값을 0으로 변경
+
+        sim_df.to_csv('error_processing.csv', index=True, header=True)
+
+    # 데이터프레임을 출력하여 0인 값에 색을 입힙니다.
+    @st.cache_data
+    def highlight_zero(val):
+        return 'background-color: yellow' if val == 0 else
+    '''
+    st.code(code, language='python')
+
+
+
+
+if selecop == '페이지 코드':
+    code ='''
+    import streamlit as st
+import pandas as pd
+import numpy as np
+from fjsp_Q import *
+import os
+import matplotlib.pyplot as plt
+
+
+# Display editor's content as you type
+st.set_page_config(page_title="data_set")
+
+col1, col2 = st.columns([8, 2])
+
+with col2:
+    st.image("tuk_img.png")
+
+st.title('데이터셋')
+
+with st.sidebar:
+    selecop = st.selectbox('설정 메뉴', ('job.csv', 'setup.csv', 'sim.csv', 'Q-time.csv','error_create.csv'))
+
+
+if selecop == 'job.csv':
+    st.subheader('job.csv파일 생성 설정')
+    csv_files = get_csv_file_list()
+    # selected_csv = st.selectbox("CSV 파일 선택", csv_files)
+    # st.write(f"선택된 CSV 파일: {selected_csv}")
+    number = st.number_input('job의 갯수',step=1)
+    number = int(number)
+    value1, value2 = st.slider('job타입의 난수범위', 0, 100, (1, 10))
+    st.write('선택범위', value1, value2)
+
+     # 파일 다운로드 이후에는 파일 이름 입력을 비활성화
+    job_time_file_name = st.text_input("파일 이름을 입력하세요 (확장자 없이):", "FJSP_Job", key="job_time_file_name")
+
+    if st.button('job.csv생성'):
+        job(number, value1, value2)
+        st.write('생성완료')
+
+        # 생성한 job.csv 파일을 읽어와서 출력
+        job_df = pd.read_csv('FJSP_Job.csv', index_col=False)
+        st.subheader(job_time_file_name + '.csv')
+        st.dataframe(job_df)
+        with open('FJSP_Job.csv') as f:
+            st.download_button(f"Download {job_time_file_name}.csv", f, file_name=f"{job_time_file_name}.csv", mime='text/csv')
+
+       
+
+if selecop == 'setup.csv':
+    st.subheader('set.csv파일 생성 설정')
+
+    value3, value4 = st.slider('setup시간 난수의 범위', 0, 100, (1, 10))
+    st.write('선택범위', value3, value4)
+
+    set_time_file_name = st.text_input("파일 이름을 입력하세요 (확장자 없이):", "FJSP_Set")
+    if st.button('setup.csv생성'):
+        setup(value3, value4)
+        setup_df = pd.read_csv('FJSP_Set.csv', index_col=False)
+        st.header(set_time_file_name + ".csv")
+        st.write(setup_df)
+        
+        with open('FJSP_Set.csv') as f:
+            st.download_button(f"Download {set_time_file_name}.csv", f, file_name=f"{set_time_file_name}.csv", mime='text/csv')
+
+number2=0 
+value5=0 
+value6=0
+value7=0
+value8=0
+if selecop == 'sim.csv':
+    st.subheader('sim.csv파일 생성 설정')
+
+    number2 = st.number_input('기계의 갯수')
+    number2 = int(number2)
+    value5, value6 = st.slider('processtime의 난수범위', 0, 100, (1, 10))
+    st.write('선택범위', value5, value6)
+
+    value7, value8 = st.slider('job_operation의 난수범위', 0, 100, (1, 10))
+    st.write('선택범위', value7, value8)
+    processing_time_file_name = st.text_input("파일 이름을 입력하세요 (확장자 없이):", "FJSP_Sim")
+    if st.button('sim.csv생성'):
+        sim(number2, value5, value6, value7, value8)
+        sim_df = pd.read_csv('FJSP_Sim.csv', index_col=False)
+        st.header(processing_time_file_name + ".csv")
+        st.write(sim_df)
+        
+        with open('FJSP_Sim.csv') as f:
+            st.download_button(f"Download {processing_time_file_name}.csv", f, file_name=f"{processing_time_file_name}.csv", mime='text/csv')
+#job_df_op = sim(number2, value5, value6, value7, value8)
+
+
+if selecop == 'Q-time.csv':
+    st.subheader('Q-time.csv 생성 설정')
+
+    # Q_time 생성 입력값 
+    q_range_min, q_range_max = st.slider('Q-time에 적용될 배수 범위', 0.0, 5.0, (1.5, 2.0))
+    st.write('선택범위', q_range_min, q_range_max)
+    Q_time_file_name = st.text_input("파일 이름을 입력하세요 (확장자 없이):", "FJSP_Q_time")
+    if st.button('Q-time 생성'):
+
+        Q_time(q_range_min, q_range_max)
+        q_time_df = pd.read_csv('FJSP_Q_time.csv', index_col=False)
+        st.header(Q_time_file_name+".csv")
+        st.write(q_time_df)
+
+        
+        with open('FJSP_Q_time.csv') as f:
+            st.download_button(f"Download {Q_time_file_name}.csv", f, file_name=f"{Q_time_file_name}.csv", mime='text/csv')
+
+    # sim.csv 생성 이후에 Q_time.csv 생성
+
+#error값 범위 지정을 위한 코드
+sim_df = pd.read_csv('FJSP_Sim.csv', index_col=0)
+
+job_pro = sim_df
+job_pro_index = job_pro.index
+counts = []
+current_count = 1
+
+for r in range(1, len(job_pro_index)):
+    if job_pro_index[r][:3] == job_pro_index[r-1][:3]:
+        current_count += 1
+    else:
+        counts.append(current_count)
+        current_count = 1
+
+counts.append(current_count)
+job_df_op = counts
+job_df_op_count = int(len(job_df_op))
+
+unavailable_machine_options = []
+machine_list = job_pro.columns
+machine_num_list = machine_list.str.replace("M","")
+machine_max = int(max(machine_num_list))
+
+
+
+
+if selecop == 'error_create.csv':
+    st.subheader('error설정')   
+    num_inputs = st.number_input('기계와 작업을 입력할 갯수', min_value=1, max_value=100, value=1, step=1)
+    unavailable_machine_options = []
+    op_number = None 
+    for idx in range(num_inputs):
+        o_count=0
+        col_m, col_j, col_o= st.columns([3, 3, 3])
+        with col_m:
+            machine_number = st.number_input('기계 지정', key=f'machine_{idx}', min_value=1,max_value= machine_max, step=1)
+            machine_number = int(machine_number)
+            st.write("지정된 기계: "+str(machine_number))
+        with col_j:
+            job_number = st.number_input('작업 지정', key=f'job_{idx}', min_value=1,max_value= job_df_op_count, step=1)
+            job_number = int(job_number)
+            st.write("지정된 작업: "+str(job_number))
+            if job_number is not None:
+                job_df_op_select = job_df_op[job_number-1]
+                with col_o:
+                    use_op_checkbox = st.checkbox('공정 지정', key=f'op_checkbox_{idx}')
+         
+                    if use_op_checkbox:
+                        op_number = st.number_input('공정 지정',key=f'op_{idx}', min_value=1,max_value =job_df_op_select, step=1)
+                        op_number = int(op_number)
+                        st.write("지정된 공정: "+str(op_number))
+                    else:
+                        op_number = None
+            else:
+                st.write("없어용")
+        
+        unavailable_machine_options.append([machine_number, job_number, op_number])
+    
+ 
+    unavailable_machine_options_pd = pd.DataFrame(unavailable_machine_options, columns=['기계', '작업', '공정'])
+
+    error_file_name = st.text_input("파일 이름을 입력하세요 (확장자 없이):", "FJSP_error_processing")
+    
+    selecop_er = st.selectbox('', ('', '문제 리스트', 'error_processing_time.csv'))
+
+    
+
+    if selecop_er == '문제 리스트':
+        st.write(unavailable_machine_options_pd)
+    if selecop_er == 'error_processing_time.csv':
+        add_unavailable_machines_to_sim("FJSP_Sim.csv", unavailable_machine_options)
+        error_processing_df = pd.read_csv('error_processing.csv', index_col=False)
+        
+        st.header(error_file_name+".csv")
+        error_processing_styled_df = error_processing_df.style.applymap(highlight_zero)
+        st.write(error_processing_styled_df)
+        st.write(error_processing_df.shape)
+
+        
+        with open('error_processing.csv') as f:
+            st.download_button(f"Download {error_file_name}.csv", f, file_name=f"{error_file_name}.csv", mime='text/csv')
+'''
+    st.code(code, language='python')
